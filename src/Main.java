95c7f3ac01070bd4c35163a1a6bfb2e587d11aa5
oyugijune <oyugijune863@gmail.com>
2023-04-16 14:19:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+
+        System.out.println("Hello world");
+        print("Hello world 2");
+    }
+    public static void print(Object obj) {
+        System.out.println(obj);
+    }
+}
